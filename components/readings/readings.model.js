6f5c40782985c80ed82074e787c3edd66358b874
Michał Winiarski <m.winiarski.z@gmail.com>
2018-12-01 14:21:51 +0100

    Fixed TTL

--- components/readings/readings.model.js
+++ components/readings/readings.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const readingSchema = new mongoose.Schema({
   temperature: { type: Number, required: true },
   sensor: { type: String, required: true },
-  created: { type: Date, expires: 86400, default: Date.nowtype },
+  created: { type: Date, expires: 86400, default: Date.now },
 })
 
 module.exports = mongoose.model('Reading', readingSchema)
