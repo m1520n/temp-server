e0a9d8a1995ff52db401ee20cabcd7f6e9697cd6
Michał Winiarski <m.winiarski.z@gmail.com>
2018-12-01 20:46:18 +0100

    Added limit to reading endpoint

--- components/readings/readings.controller.js
+++ components/readings/readings.controller.js
@@ -3,6 +3,7 @@ const Reading = require('./readings.model')
 module.exports = {
   getReadings: async (req, res) => {
     const { limit } = req.query;
+    console.log(req.query);
     const readings = await Reading.find({}).sort({ createdAt: -1 }).limit(limit || 100)
 
     res.json({
