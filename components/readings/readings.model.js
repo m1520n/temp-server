365929a081f2ec1e399d5e9e3c6fde331da38456
Michał Winiarski <m.winiarski.z@gmail.com>
2018-12-02 11:53:06 +0100

    Added humidity and sensorId fields to reading schema

--- components/readings/readings.model.js
+++ components/readings/readings.model.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema
 
 const readingSchema = new mongoose.Schema({
   temperature: { type: Number, required: true },
+  humidity: { Type: Number},
   sensor: { type: String, required: true },
+  sensorId: { tpye: Number, required: true },
   createdAt: { type: Date, expires: 86400000, default: Date.now },
-})
+});
 
 module.exports = mongoose.model('Reading', readingSchema)
 
